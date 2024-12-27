5e93831a802be8a7099210b8607164b46e7b5e7a
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-12-27 22:04:18 +0530

    Update wsgi.py

--- MyWeb/wsgi.py
+++ MyWeb/wsgi.py
@@ -1,14 +1,9 @@
-"""
-WSGI config for MyWeb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
+import os
 from django.core.wsgi import get_wsgi_application
 
-# This is the handler Vercel looks for
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWeb.settings')
+
+# Define the handler for Vercel
 handler = get_wsgi_application()
 
 
