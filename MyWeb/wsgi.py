5992a68136e53b979802d7a636c2a75f8c60f32b
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-12-27 21:49:37 +0530

    imported wsgi

--- MyWeb/wsgi.py
+++ MyWeb/wsgi.py
@@ -6,11 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
-import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWeb.settings')
+# This is the handler Vercel looks for
+handler = get_wsgi_application()
 
-application = get_wsgi_application()
+
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyWeb.settings')
+
+# application = get_wsgi_application()
