4942093cac013f691d994905e2b4ee8760d05717
Samyak Jain <115931952+samyckk@users.noreply.github.com>
2024-12-27 22:10:28 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.wsgi import get_wsgi_application
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,8 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+# Define the handler for Vercel
+handler = get_wsgi_application()
 
 if __name__ == '__main__':
     main()
